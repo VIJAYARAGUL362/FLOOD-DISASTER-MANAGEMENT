f29b33b4d404ff48e3fc828e33fad8a51a6bf72f
VIJAYARAGUL362 <wearevr2005@gmail.com>
2025-07-10 21:44:43 +0530

    updated main for two models

--- main.py
+++ main.py
@@ -13,11 +13,23 @@ class PredictionInput(BaseModel):
 with open('Random_forest_model.pkl', 'rb') as file:
     model = joblib.load(file)
 
+with open('Random_forest_model.pkl', 'rb') as file:
+    model2 = joblib.load(file)
+
 @app.post("/predict")
 async def predict(input_data: PredictionInput):
     try:
         features = np.array([input_data.features])
         prediction = model.predict(features)
+        return {"prediction": prediction.tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/predict/damage")
+async def predict(input_data: PredictionInput):
+    try:
+        features = np.array([input_data.features])
+        prediction = model2.predict(features)
         return {"prediction": prediction.tolist()}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
